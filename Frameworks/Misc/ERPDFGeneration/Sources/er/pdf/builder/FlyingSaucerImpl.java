77670e498414773f73e1e3d4bae8942872367d09
Steve <spalmer@desertsky.com>
2015-08-25 11:47:36 -0700

    Removed unuse imports.

--- Frameworks/Misc/ERPDFGeneration/Sources/er/pdf/builder/FlyingSaucerImpl.java
+++ Frameworks/Misc/ERPDFGeneration/Sources/er/pdf/builder/FlyingSaucerImpl.java
@@ -2,10 +2,7 @@ package er.pdf.builder;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.Reader;
-import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 
